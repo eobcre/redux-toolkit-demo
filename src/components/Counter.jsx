241448c96a0c0d94bba3f1b6a9eec86bc0360cb4
eobcre <eobcre@gmail.com>
2023-05-19 10:18:26 -0400

    counter layout created

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,15 @@
-const Counter = () => {
-  return <div></div>;
-};
+import CounterResult from './CounterResult';
+import CounterButton from './CounterButton';
 
+const Counter = () => {
+  return (
+    <>
+      <CounterResult />
+      <CounterButton step={2} calcType='+' />
+      <CounterButton step={2} calcType='-' />
+      <CounterButton step={10} calcType='+' />
+      <CounterButton step={10} calcType='-' />
+    </>
+  );
+};
 export default Counter;
