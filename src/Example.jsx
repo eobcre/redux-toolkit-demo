141d7e8c3c7343ccb10870fb622d076a19e84c04
eobcre <eobcre@gmail.com>
2023-05-19 09:46:29 -0400

    provider created to connect store and react

--- src/Example.jsx
+++ src/Example.jsx
@@ -1,5 +1,15 @@
+import { Provider } from 'react-redux';
+import store from './store/store';
+import Counter from './components/Counter';
+
 const Example = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    </div>
+  );
 };
 
 export default Example;
